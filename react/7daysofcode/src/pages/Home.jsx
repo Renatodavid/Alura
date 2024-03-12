79b99519e59cd5ebe2d9dbcc6b091443802bde6f
= <renatomdavid811@hotmail.com>
2024-03-12 20:28:02 -0300

    login concluido

--- react/7daysofcode/src/pages/Home.jsx
+++ react/7daysofcode/src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import { App } from "../layouts/App";
 
+
 export const Home = () => (
   <App>
-    <h1 className="text-xl text-sky-600">Home</h1>
+    
+    <div className="container">
+    <h1 className="text-xl text-sky-600">Login</h1>
+      <form className="forms" type="submit">
+        <label className="text " htmlFor="email">Digite o seu melhor e-mail</label>
+        <input type="text" placeholder="email@exemplo.com" />
+
+       <label className="text" htmlFor="password">Senha</label>
+        <input type="password" placeholder="********" />
+
+        <button type="submit">Acessar conta</button>
+        
+        
+        <span className="text-sm">Não possui uma conta? <a href="#" >Crie uma agora!</a></span>
+      </form>
+     
+      </div>
+      
+  
   </App>
 );
